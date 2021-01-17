dd1690f17496a0739617f041ef3feb4b20b039b8
RadenMahar <panji@alterra.id>
2021-01-17 10:36:56 +0700

    init project

---
+++ poke-app/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Blog App</title>
+      </Head>
+      <h1>Blog</h1>
+    </div>
+  )
+}
+export default Home;
